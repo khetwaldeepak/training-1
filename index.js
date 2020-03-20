073d1dda53a108bbf003dfd4edc9dc57aeb7c6bd
khetwaldeepak <58570700+khetwaldeepak@users.noreply.github.com>
2020-03-20 10:36:15 -0400

    Adding index.js file

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    }; 
+    try{
+       const sql = require("mysql");
+       //const https = require('https')
+    } catch(err){
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify(err),
+        }; 
+    }
+    
+    var config = {
+        user: 'deepakkhetwal',
+        password: 'admin!123',
+        server: 'onetskills.database.windows.net', 
+        database: 'onetskillsextractor' ,
+        options: {
+            encrypt: true  // use this for Azure database encryption
+          }
+    };
+    /*sql.connect(config, function (err) {
+        console.error(err);
+        /*if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from ONETSKILLS', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+           //return recordset; 
+           const response = {
+            statusCode: 200,
+            body: JSON.stringify('Hello from Lambda!'),
+            };
+            
+        }); 
+        
+    });  */
+   return response;
+/*try{
+    
+    
+   
+    }
+    catch(err){
+         const response = {
+            statusCode: 200,
+            body: JSON.stringify(err),
+            };
+    } 
+    return response; */
+    
+};
